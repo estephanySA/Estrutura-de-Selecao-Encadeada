74eaf46708fd9ee9b9c95bc3197b279c2fdca389
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-16 20:18:42 -0300

    Exercicio 02 finalizado

---
+++ src/Exercicio02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int diaria;
+        double conta, taxa;
+
+        System.out.println("total de diárias: ");
+        diaria = sc.nextInt();
+
+        if (diaria > 15){
+            taxa = 15.50;
+        }
+
+        else if ( diaria == 15) {
+            taxa = 36;
+        }
+        else {
+            taxa = 58;
+        }
+
+        conta = diaria * ( 250 + taxa );
+
+        System.out.println("Total das suas diarias será de: " + conta);
+
+
+
+    }
+}
